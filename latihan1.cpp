70f70d4e0e737b4872799f84ad80f621997216d2
arifrahmadi23 <muhammad231arif@gmail.com>
2023-03-06 15:32:36 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,23 @@
+//library
+#include <iostream>
+using namespace std;
+
+//program main
+int main(){//begin
+    
+    //numeric p,l,luas
+    double p,l,luas;
+    //display "masukan panjang"
+    cout << "masukan panjang = ";
+    //accept p
+    cin >> p;
+    //display "masukan lebar"
+    cout << "masukan lebar = ";
+    //accept l
+    cin >> l;
+    //compute luas=p*l
+    luas=p*l;
+    //display "luas persegi panjang", luas
+    cout << "luas persegi panjang adalah "<<luas;
+
+}//end
---
+++ projek1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "Selamat Datang" << endl;
+    cout << "Menghitung Luas Persegi" << endl;
+    cout << "Panjang = " << endl;
+     
+
+    return 0;
+}
